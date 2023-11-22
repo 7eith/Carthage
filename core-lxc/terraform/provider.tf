498f777f9e169b89411ae3409b7dee19f7975a0d
Alexis 'seith' M <amontelimart@gmail.com>
2023-11-22 09:51:35 +0100

    :hammer: ref: use var for pm_api_url

--- core-lxc/terraform/provider.tf
+++ core-lxc/terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "proxmox" {
     pm_debug = true
-    pm_api_url = "https://192.168.1.42:8006/api2/json"
+    pm_api_url = var.proxmox_url
     pm_api_token_id =  var.pm_api_token_id
     pm_api_token_secret = var.pm_api_token_secret
     pm_tls_insecure = true 
